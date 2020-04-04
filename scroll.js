c4fe8e6002fc256551ea3443de20dbe84c35043e
Chris Pence <cpence1@students.towson.edu>
2020-04-04 19:17:15 -0400

    4/4

--- scroll.js
+++ scroll.js
@@ -3,7 +3,7 @@ function Scroll(){
     if(!document.getElementById) return;
     obj=document.getElementById("thetext");
     pos--;
-    if(pos<0-obj.offsetHeight+130) return;
+    if(pos<0-obj.offsetHeight) return;
     obj.style.top=pos;
     window.setTimeout("Scroll();",30);
 }
